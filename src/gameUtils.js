1bda3df4d094232b6e1ed0e65f3a6f25a767211b
duhee <duhee@groufin.com>
2025-02-18 19:24:05 +0900

    feat: 3. 업/다운 힌트 및 정답 제공 기능 추가

--- src/gameUtils.js
+++ src/gameUtils.js
@@ -1,3 +1,9 @@
 export function createRandomNumber() {
     return Math.floor(Math.random() * 50) + 1;
 }
+
+export function checkGuess(guess, answer) {
+    if (guess > answer) return "다운";
+    if (guess < answer) return "업";
+    return "정답!";
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import { createRandomNumber } from "./gameUtils.js";
+import { createRandomNumber, checkGuess } from "./gameUtils.js";
 import { getUserInput } from "./inputUtils.js";
 
 
 async function playGame() {
 
+    let currentTries = 0;
+    let previousGuesses = [];
+
     const answer = createRandomNumber();
     console.log("컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.");
 
     const inputNum = await getUserInput();
+    currentTries++;
+    previousGuesses.push(inputNum);
+
+    const result = checkGuess(inputNum, answer);
+    console.log(result);
+    if (result === "정답!")
+        console.log(`축하합니다! ${currentTries}번 만에 숫자를 맞추셨습니다.`);
+    else
+        console.log(`이전 추측: ${previousGuesses.join(", ")}`);
+
 }
 
 playGame();
