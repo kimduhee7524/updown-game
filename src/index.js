03aabaa1e46cbbdfd9fd00ddc942ca4a47cc96a8
duhee <duhee@groufin.com>
2025-02-18 19:14:33 +0900

    feat: 2. 사용자 입력 처리 기능 추가

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { createRandomNumber } from "./gameUtils.js";
+import { getUserInput } from "./inputUtils.js";
 
 
 async function playGame() {
@@ -6,6 +7,7 @@ async function playGame() {
     const answer = createRandomNumber();
     console.log("컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.");
 
+    const inputNum = await getUserInput();
 }
 
 playGame();
